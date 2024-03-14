8070e6bb8e6e380dae65783c34f2e3b122354115
Flame <5017_30@mail.ru>
2024-03-15 00:18:05 +0300

    refactor

--- WorldSkills_django_site/WorldSkills_django_site/urls.py
+++ WorldSkills_django_site/WorldSkills_django_site/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 
-from women.views import WomenApiView
+from women.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/womenlist/', WomenApiView.as_view()),
-    path('api/v1/women/', WomenApiView.as_view()),
-    path('api/v1/women/<int:pk>/', WomenApiView.as_view()),
+    path('api/v1/women/list', WomenAPIList.as_view()),
+    path('api/v1/women/create', WomenAPICrete.as_view()),
+    path('api/v1/women/<int:pk>/', WomenAPIRetrieve.as_view()),
+    path('api/v1/women/update/<int:pk>', WomenAPIUpdate.as_view()),
+    path('api/v1/women/delete/<int:pk>', WomenAPIDelete.as_view()),
 ]
--- WorldSkills_django_site/women/views.py
+++ WorldSkills_django_site/women/views.py
@@ -1,60 +1,29 @@
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics
 
 from .models import Women
 from .serializers import WomenSerializer
 
 
-class WomenApiView(APIView):
-    def get(self, request, pk=None):
-        if pk is not None:
-            try:
-                woman = Women.objects.get(pk=pk)
-                serializer = WomenSerializer(woman)
-                return Response(serializer.data)
-            except Women.DoesNotExist:
-                return Response({"error": "Object does not exist"}, status=404)
-        else:
-            data = Women.objects.all()
-            return Response({"women": WomenSerializer(data, many=True).data})
+class WomenAPIList(generics.ListAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
 
-    def post(self, request):
-        serializer = WomenSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
 
-        women_new = Women.objects.create(
-            title=request.data['title'],
-            content=request.data['content'],
-            cat_id=request.data['cat_id']
-        )
+class WomenAPIRetrieve(generics.RetrieveAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
 
-        return Response({"women": WomenSerializer(women_new).data})
 
-    def put(self, request, *args, **kwargs):
-        pk = kwargs.get('pk', None)
-        if not pk:
-            return Response({"error": "PK is required"}, status=400)
+class WomenAPICrete(generics.CreateAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
 
-        try:
-            instance = Women.objects.get(pk=pk)
-        except Women.DoesNotExist:
-            return Response({"error": "Object does not exist"}, status=404)
 
-        serializer = WomenSerializer(data=request.data, instance=instance)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({"women": serializer.data}, status=200)
+class WomenAPIUpdate(generics.UpdateAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
 
-    def delete(self, request, *args, **kwargs):
-        pk = kwargs.get('pk', None)
-        if not pk:
-            return Response({"error": "PK is required"}, status=400)
 
-        try:
-            instance = Women.objects.get(pk=pk)
-            instance_id = instance.id
-        except Women.DoesNotExist:
-            return Response({"error": "Object does not exist"}, status=404)
-
-        instance.delete()
-        return Response({"message": f"Record with ID {instance_id} deleted"}, status=200)
+class WomenAPIDelete(generics.RetrieveDestroyAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
